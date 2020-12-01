b4b24d8c72dbf32031357e3057a55cf3aeea0a6a
TomBitDe <tbit455674@gmail.com>
2020-12-01 20:30:05 +0100

    Added more testcases.

--- IACH-7-ejb/test/com/mycompany/iach7/tour/ToureventManagerBeanNGTest.java
+++ IACH-7-ejb/test/com/mycompany/iach7/tour/ToureventManagerBeanNGTest.java
@@ -4,10 +4,13 @@ import com.mycompany.iach7.tour.entity.Eventstat;
 import com.mycompany.iach7.tour.entity.Lap;
 import com.mycompany.iach7.tour.entity.LapPK;
 import com.mycompany.iach7.tour.entity.Lapeventtype;
+import com.mycompany.iach7.tour.entity.Prio;
 import com.mycompany.iach7.tour.entity.Tour;
 import com.mycompany.iach7.tour.entity.Tourevent;
 import com.mycompany.iach7.tour.entity.ToureventPK;
 import com.mycompany.iach7.tour.entity.Tourstat;
+import com.mycompany.iach7.util.dttm.DttmCalc;
+import com.mycompany.iach7.util.dttm.DttmMakeHelper;
 import java.util.List;
 import javax.ejb.embeddable.EJBContainer;
 import static org.testng.Assert.assertEquals;
@@ -31,6 +34,7 @@ public class ToureventManagerBeanNGTest {
     private static final String OTHER_TRUCKER_ID = "EGON";
     private static final String OTHER_AGENT_ID = "FEDEX";
     private static final String OTHER_CUSTOMER_ID = "SYMRISE";
+    private static final String TSTU = "Test";
 
     private static EJBContainer container;
 
@@ -45,9 +49,11 @@ public class ToureventManagerBeanNGTest {
     @BeforeClass
     public static void setUpClass() throws Exception {
         container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
+
         tour = (TourManager) container.getContext().lookup("java:global/classes/TourManagerBean");
         lap = (LapManager) container.getContext().lookup("java:global/classes/LapManagerBean");
 
+        // Start creating some test data
         Tour expResult = new Tour(OTHER_TOUR_ID, Tourstat.finished,
                                   OTHER_TRUCK_ID, OTHER_TRUCKER_ID,
                                   OTHER_AGENT_ID);
@@ -63,6 +69,12 @@ public class ToureventManagerBeanNGTest {
         Lap resultLap = new Lap(OTHER_TOUR_ID, "DUS");
         resultLap.setCustomer(OTHER_CUSTOMER_ID);
         resultLap.setLapSeqn(1);
+        resultLap.setEta(DttmMakeHelper.makeDttm());
+        resultLap.setGate("A25");
+        resultLap.setUnldur("60000");
+        resultLap.setLapPrio(Prio.low);
+        resultLap.setAgentSta(DttmMakeHelper.makeDttm());
+        resultLap.setSyncSta(DttmMakeHelper.makeDttm());
 
         lap.create(resultLap);
 
@@ -74,6 +86,12 @@ public class ToureventManagerBeanNGTest {
         resultLap = new Lap(OTHER_TOUR_ID, "FRA");
         resultLap.setCustomer(OTHER_CUSTOMER_ID);
         resultLap.setLapSeqn(2);
+        resultLap.setEta(DttmCalc.dttmAdd14(DttmMakeHelper.makeDttm(), 600000));
+        resultLap.setGate("B4");
+        resultLap.setUnldur("80000");
+        resultLap.setLapPrio(Prio.low);
+        resultLap.setAgentSta(DttmMakeHelper.makeDttm());
+        resultLap.setSyncSta(DttmMakeHelper.makeDttm());
 
         lap.create(resultLap);
 
@@ -85,6 +103,12 @@ public class ToureventManagerBeanNGTest {
         resultLap = new Lap(OTHER_TOUR_ID, "BUD");
         resultLap.setCustomer(OTHER_CUSTOMER_ID);
         resultLap.setLapSeqn(3);
+        resultLap.setEta(DttmCalc.dttmAdd14(DttmMakeHelper.makeDttm(), 1200000));
+        resultLap.setGate("A12");
+        resultLap.setUnldur("45000");
+        resultLap.setLapPrio(Prio.normal);
+        resultLap.setAgentSta(DttmMakeHelper.makeDttm());
+        resultLap.setSyncSta(DttmMakeHelper.makeDttm());
 
         lap.create(resultLap);
 
@@ -124,6 +148,19 @@ public class ToureventManagerBeanNGTest {
     public void testAgentStaChange() {
         System.out.println("agentStaChange");
 
+        // Really change the Agent STA
+        Tourevent result = tourevent.agentStaChange(new LapPK(OTHER_TOUR_ID, "DUS"),
+                                                    TSTU,
+                                                    DttmCalc.dttmAdd14(DttmMakeHelper.makeDttm(), 60000));
+        assertNotNull(result);
+
+        // Don't change the Agent STA
+        String agentSta = lap.getById(new LapPK(OTHER_TOUR_ID, "DUS")).getAgentSta();
+
+        result = tourevent.agentStaChange(new LapPK(OTHER_TOUR_ID, "DUS"),
+                                          TSTU,
+                                          agentSta);
+        assertNull(result);
     }
 
     /**
@@ -133,6 +170,19 @@ public class ToureventManagerBeanNGTest {
     public void testCustomerStaChange() {
         System.out.println("customerStaChange");
 
+        // The Customer STA has not been set before (#see setUpClass)
+        Tourevent result = tourevent.customerStaChange(new LapPK(OTHER_TOUR_ID, "DUS"),
+                                                       TSTU,
+                                                       DttmMakeHelper.makeDttm());
+        assertNotNull(result);
+
+        // Don't change the Customer STA
+        String customerSta = lap.getById(new LapPK(OTHER_TOUR_ID, "DUS")).getCustomerSta();
+
+        result = tourevent.customerStaChange(new LapPK(OTHER_TOUR_ID, "DUS"),
+                                             TSTU,
+                                             customerSta);
+        assertNull(result);
     }
 
     /**
@@ -142,6 +192,18 @@ public class ToureventManagerBeanNGTest {
     public void testSyncStaChange() {
         System.out.println("syncStaChange");
 
+        Tourevent result = tourevent.syncStaChange(new LapPK(OTHER_TOUR_ID, "DUS"),
+                                                   TSTU,
+                                                   DttmMakeHelper.makeDttm());
+        assertNotNull(result);
+
+        // Don't change the Sync STA
+        String syncSta = lap.getById(new LapPK(OTHER_TOUR_ID, "DUS")).getSyncSta();
+
+        result = tourevent.syncStaChange(new LapPK(OTHER_TOUR_ID, "DUS"),
+                                         TSTU,
+                                         syncSta);
+        assertNull(result);
     }
 
     /**
@@ -151,6 +213,18 @@ public class ToureventManagerBeanNGTest {
     public void testEtaChange() {
         System.out.println("etaChange");
 
+        Tourevent result = tourevent.etaChange(new LapPK(OTHER_TOUR_ID, "DUS"),
+                                               TSTU,
+                                               DttmCalc.dttmAdd14(DttmMakeHelper.makeDttm(), 600000));
+        assertNotNull(result);
+
+        // Don't change the ETA
+        String eta = lap.getById(new LapPK(OTHER_TOUR_ID, "DUS")).getEta();
+
+        result = tourevent.etaChange(new LapPK(OTHER_TOUR_ID, "DUS"),
+                                     TSTU,
+                                     eta);
+        assertNull(result);
     }
 
     /**
@@ -160,6 +234,17 @@ public class ToureventManagerBeanNGTest {
     public void testUnloadDurationChange() {
         System.out.println("unloadDurationChange");
 
+        Tourevent result = tourevent.unloadDurationChange(new LapPK(OTHER_TOUR_ID, "DUS"),
+                                                          TSTU, "90");
+        assertNotNull(result);
+
+        // Don't change the unload duration
+        String unlDur = lap.getById(new LapPK(OTHER_TOUR_ID, "DUS")).getUnldur();
+
+        result = tourevent.unloadDurationChange(new LapPK(OTHER_TOUR_ID, "DUS"),
+                                                TSTU,
+                                                unlDur);
+        assertNull(result);
     }
 
     /**
@@ -169,6 +254,17 @@ public class ToureventManagerBeanNGTest {
     public void testGateChange() {
         System.out.println("gateChange");
 
+        Tourevent result = tourevent.gateChange(new LapPK(OTHER_TOUR_ID, "DUS"),
+                                                TSTU, "B17");
+        assertNotNull(result);
+
+        // Don't change the unload duration
+        String gate = lap.getById(new LapPK(OTHER_TOUR_ID, "DUS")).getGate();
+
+        result = tourevent.gateChange(new LapPK(OTHER_TOUR_ID, "DUS"),
+                                      TSTU,
+                                      gate);
+        assertNull(result);
     }
 
     /**
@@ -178,15 +274,22 @@ public class ToureventManagerBeanNGTest {
     public void testLapArrivalReported() {
         System.out.println("lapArrivalReported");
 
+        Tourevent result = tourevent.lapArrivalReported(new LapPK(OTHER_TOUR_ID, "DUS"),
+                                                        TSTU);
+        assertNotNull(result);
     }
 
     /**
      * Test of setupEta method, of class ToureventManagerBean.
      */
-    @Test
+    @Test(priority = 1)
     public void testSetupEta() {
         System.out.println("setupEta");
 
+        Tourevent result = tourevent.setupEta(new LapPK(OTHER_TOUR_ID, "DUS"),
+                                              TSTU,
+                                              DttmMakeHelper.makeDttm());
+        assertNotNull(result);
     }
 
     /**
@@ -196,6 +299,8 @@ public class ToureventManagerBeanNGTest {
     public void testGetAll() {
         System.out.println("getAll");
 
+        // Since delete has not been called (priority = 99)
+        // some objects should be in the database
         List<Tourevent> result = tourevent.getAll();
         assertFalse(result.isEmpty());
     }
@@ -257,28 +362,28 @@ public class ToureventManagerBeanNGTest {
     public void testEventInit() {
         System.out.println("eventInit");
 
-        Tourevent result = tourevent.eventInit(new LapPK(OTHER_TOUR_ID, "DUS"), "Test");
+        Tourevent result = tourevent.eventInit(new LapPK(OTHER_TOUR_ID, "DUS"), TSTU);
         assertNotNull(result);
         assertEquals(result.getId().getTourId(), OTHER_TOUR_ID);
         assertEquals(result.getId().getLapdest(), "DUS");
         assertEquals(result.getEventstat(), Eventstat.created);
         assertEquals(result.getLapeventtype(), Lapeventtype.hopinit);
 
-        result = tourevent.eventInit(new LapPK(OTHER_TOUR_ID, "FRA"), "Test");
+        result = tourevent.eventInit(new LapPK(OTHER_TOUR_ID, "FRA"), TSTU);
         assertNotNull(result);
         assertEquals(result.getId().getTourId(), OTHER_TOUR_ID);
         assertEquals(result.getId().getLapdest(), "FRA");
         assertEquals(result.getEventstat(), Eventstat.created);
         assertEquals(result.getLapeventtype(), Lapeventtype.hopinit);
 
-        result = tourevent.eventInit(new LapPK(OTHER_TOUR_ID, "BUD"), "Test");
+        result = tourevent.eventInit(new LapPK(OTHER_TOUR_ID, "BUD"), TSTU);
         assertNotNull(result);
         assertEquals(result.getId().getTourId(), OTHER_TOUR_ID);
         assertEquals(result.getId().getLapdest(), "BUD");
         assertEquals(result.getEventstat(), Eventstat.created);
         assertEquals(result.getLapeventtype(), Lapeventtype.hopinit);
 
-        result = tourevent.eventInit(new LapPK(OTHER_TOUR_ID, "DXB"), "Test");
+        result = tourevent.eventInit(new LapPK(OTHER_TOUR_ID, "DXB"), TSTU);
         assertNull(result);
     }
 }
