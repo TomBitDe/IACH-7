b50959ae48f594e25f11dd763d3bff64d4d2c018
TomBitDe <tbit455674@gmail.com>
2020-12-08 18:54:45 +0100

    Bugfix in documentation.

--- IACH-7-ejb/src/java/com/mycompany/iach7/slatime/entity/package-info.java
+++ IACH-7-ejb/src/java/com/mycompany/iach7/slatime/entity/package-info.java
@@ -5,6 +5,8 @@
  * Also containing the special types (enums) associated with the business objects. All business objects are loosley
  * connected. The connection is done via the business logic.<br>
  * <p>
+ * Here the ManyToOne and OneToMany dependency between ServiceLevelAgreement is introduced. See the description below.
+ * <p>
  * A ServiceLevelAgreement (SLA) is the contract type.<br>
  * A SlaTime is the time to fullfil a contract type<br>
  * One SLA is assigned to 0..N SlaTime. An SlaTime has 0 or 1 SLA. This is defined as @OneToMany and @ManyToOne
